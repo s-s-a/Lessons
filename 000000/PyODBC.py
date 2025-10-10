a9818e16dac6a63ff03ab8022046a96a20576f15
Sergey <sssss_aaaaa_2011@mail.ru>
2025-10-10 21:22:44 +0300

    1

--- 000000/PyODBC.py
+++ 000000/PyODBC.py
@@ -1,23 +1,40 @@
-import pyodbc
+import mssql_python as pyodbc # pip install mssql-python
 
-conn = pyodbc.connect(r'''Driver={ODBC Driver 17 for SQL Server};
-<<<<<<< HEAD
-                      Server=.;
-=======
-                      Server=msk-db-1;
->>>>>>> 5c39f649394f144af54e86d1663d6d480dab1841
-                      Database=master;
+SERVER_NAME = "msk-db-1"
+DB_NAME = "ItalikaSQL"
+DB_CONNECTION_STRING = r'''Driver={ODBC Driver 17 for SQL Server};
+                      Server=''' + SERVER_NAME + ''';
+                      Database=''' + DB_NAME + ''';
                       Trusted_Connection=yes;
+                      TrustServerCertificate=yes;
                       APP=Pyton App;
-                      Workstation=SSA-ASUS''')
+                      Workstation=SSA-ASUS'''
+
+
+settings = pyodbc.get_settings()
+print(f'{settings.lowercase=}')         # False
+print(f'{settings.decimal_separator=}') # "."
+
+# Enable pooling with defaults
+pyodbc.pooling()
+
+# Enable pooling with custom values
+pyodbc.pooling(max_size=50, idle_timeout=300)
+
+# Disable pooling
+pyodbc.pooling(enabled=False)
+
+
+conn = pyodbc.connect(DB_CONNECTION_STRING)
 # conn.setdecoding(pyodbc.SQL_CHAR,encoding='utf-8')
+
+
 cursor = conn.cursor()
 cursor.execute('select @@version')
 for row in cursor:
-<<<<<<< HEAD
-    print(row)
-=======
     print(str(row[0]).replace(r'\n', '\n').replace(r'\t', '\t'))
->>>>>>> 5c39f649394f144af54e86d1663d6d480dab1841
+
+cursor.execute("PRINT 'Hello world!'")
+print(f'{cursor.messages=}')
 
 conn.close()
